77e2532a9e42f19ed47c909202d5395bc9dfc75e
Prophet98k <814836297@qq.com>
2021-05-08 18:10:49 +0800

    login can work

--- app/models.py
+++ app/models.py
@@ -1,23 +1,35 @@
-from app import db
 from datetime import datetime
+from app import db
 
-class User(db.Model):
-    email = db.Column(db.String(120), index=True, primary_key=True)
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     results = db.relationship('Result', backref='author', lazy='dynamic')
 
-
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Result(db.Model):
-    username = db.Column(db.String(64), db.ForeignKey('user.username'),primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     mark = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<Result {}>'.format(self.body)
-
-
-
+        return '<Result {}>'.format(self.mark)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
+from flask import request
+from werkzeug.urls import url_parse
 
 @app.route('/')
 def welcome():
@@ -13,12 +18,19 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        data = form.data
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.email.data, form.remember_me.data))
-        return redirect('/home')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password, please try again')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register')
@@ -41,3 +53,8 @@ def accessment():
 @app.route('/table')
 def table():
     return render_template("table.html")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
