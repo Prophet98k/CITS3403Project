e364caac6606e0fe351dec75ca3cbdab99b94514
Prophet98k <814836297@qq.com>
2021-05-09 12:38:32 +0800

    question marking

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField, SelectField
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
+from app.models import User
 
 
 
@@ -10,3 +11,40 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
+class RegistrationForm(FlaskForm):
+    username = StringField('*Username:', validators=[DataRequired()])
+    email = StringField('*Email:', validators=[DataRequired(), Email()])
+    password = PasswordField('*Password:', validators=[DataRequired()])
+    password2 = PasswordField(
+        '*Repeat Password:', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+class AnswerForm(FlaskForm):
+    q1 = RadioField('1. What is the romaji of "ヌ" (such as "ga" is the romaji of "が")', choices=['no', 'nu', 'ne','mo'])
+    q2 = RadioField('2. What are the romaji of "ぬ" and "め" in sequence', choices=['numu', 'munu', 'nume','menu'])
+    q3 = RadioField('3. Which one is the katakana of “ne"', choices=['ネ', 'ね', 'レ','れ'])
+    q4 = RadioField('4. Which one is the hiragana of “ヲ"', choices=['を', 'フ', 'ふ','れ'])
+    q5 = RadioField('5. Which choices is the romaji of "が" and "カ"', choices=['ga ka', 'ga ga', 'ka ka','ka ga'])
+    q6 = StringField('6. Please input katakana of ”の"')
+    q7 = StringField('7. Please input katakana of ”へ"')
+    q8 = StringField('8. Please input hiragana of ”タ"')
+    q9 = StringField('9. Please input katakana of ”ポ"')
+    q10 = StringField('10. Please input hiragana of ”ン"')
+    submit = SubmitField('Submit')
+
+
+
+
+
+
+    
--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,11 @@ from flask_login import logout_user
 from flask import request
 from werkzeug.urls import url_parse
 
+from app import db
+from app.forms import RegistrationForm, AnswerForm
+
+
+
 @app.route('/')
 def welcome():
     return render_template("welcome.html")
@@ -19,7 +24,7 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -33,21 +38,35 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/learn')
 def learn():
     return render_template("learn.html")
 
-@app.route('/accessment_home')
-def accessment_home():
-    return render_template("accessment_home.html")
+@app.route('/assessment_home')
+def assessment_home():
+    return render_template("assessment_home.html")
 
-@app.route('/accessment')
-def accessment():
-    return render_template("accessment.html")
+@app.route('/assessment', methods=['GET', 'POST'])
+def assessment():
+    form = AnswerForm()
+    if form.validate_on_submit():
+        score,correct = getmark(form)
+        return redirect(url_for('feedback', score = score, correct = correct))
+    return render_template("assessment.html", form=form)
 
 
 @app.route('/table')
@@ -58,3 +77,43 @@ def table():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route('/feedback?<int:score><string:correct>')
+def feedback(score,correct):
+    return render_template("feedback.html", score = score, correct = correct)
+
+
+def getmark(form):
+    score = 0
+    correct = ''
+    if form.q1.data == 'nu':
+        score += 10
+        correct += ' 1'
+    if form.q2.data == 'nume':
+        score += 10
+        correct += ' 2'
+    if form.q3.data == 'ネ':
+        score += 10
+        correct += ' 3'
+    if form.q4.data == 'を':
+        score += 10
+        correct += ' 4'
+    if form.q5.data == 'ga ka':
+        score += 10
+        correct += ' 5'
+    if form.q6.data == 'ノ':
+        score += 10
+        correct += ' 6'
+    if form.q7.data == 'へ':
+        score += 10
+        correct += ' 7'
+    if form.q8.data == 'た':
+        score += 10
+        correct += ' 8'
+    if form.q9.data == 'ぽ':
+        score += 10
+        correct += ' 9'
+    if form.q10.data == 'ん':
+        score += 10
+        correct += ' 10'
+    return score, correct
